f23c7263124a1c47102b2f7922db7e606d5b74a9
Alayna Hassan <alaynahassan08@gmail.com>
2023-08-15 20:17:36 +0200

    Initial Commit

---
+++ GlowyFinalProject/GlowyFinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlowyFinalProjectApp.swift
+//  GlowyFinalProject
+//
+//  Created by Alayna Hassan on 2023-08-15.
+//
+
+import SwiftUI
+
+@main
+struct GlowyFinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
